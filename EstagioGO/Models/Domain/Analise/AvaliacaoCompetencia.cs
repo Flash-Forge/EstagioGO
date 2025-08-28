221bceb68f2e68e7d08bafbf8a5bb52a01a68626
Tyxiel <128870773+Tyxiel@users.noreply.github.com>
2025-08-28 09:49:10 -0300

    Cria os modelos basicos de avaliacao

---
+++ EstagioGO/Models/Domain/Analise/AvaliacaoCompetencia.cs
@@ -0,0 +1,29 @@
+﻿    using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EstagioGO.Models.Domain.Analise
+{
+    public class AvaliacaoCompetencia
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int AvaliacaoId { get; set; }
+
+        [ForeignKey("AvaliacaoId")]
+        public virtual Avaliacao Avaliacao { get; set; }
+
+        [Required]
+        public int CompetenciaId { get; set; }
+
+        [ForeignKey("CompetenciaId")]
+        public virtual Competencia Competencia { get; set; }
+
+        [Required(ErrorMessage = "A nota é obrigatória")]
+        [Range(0, 5, ErrorMessage = "A nota deve estar entre 0 e 5")]
+        public int Nota { get; set; }
+
+        [StringLength(500, ErrorMessage = "O comentário não pode ter mais de 500 caracteres")]
+        public string Comentario { get; set; }
+    }
+}
